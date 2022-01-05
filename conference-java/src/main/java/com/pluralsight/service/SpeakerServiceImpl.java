804bc3c7e65261c49b6d11f66177163503b08fae
llullanqui <luis.1142@hotmail.com>
2022-01-04 22:47:11 -0500

    First Autowiring

--- conference-java/src/main/java/com/pluralsight/service/SpeakerServiceImpl.java
+++ conference-java/src/main/java/com/pluralsight/service/SpeakerServiceImpl.java
@@ -3,14 +3,23 @@ package com.pluralsight.service;
 import com.pluralsight.model.Speaker;
 import com.pluralsight.repository.HibernateSpeakerRepositoryImpl;
 import com.pluralsight.repository.SpeakerRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 
+@Service("speakerService")
 public class SpeakerServiceImpl implements SpeakerService {
 
     private SpeakerRepository repository;
 
+    public SpeakerServiceImpl() {
+        System.out.println("SpeakerServiceImpl no args constructor");
+    }
+
+    @Autowired
     public SpeakerServiceImpl(SpeakerRepository speakerRepository) {
+        System.out.println("SpeakerServiceImpl repository constructor");
         repository = speakerRepository;
     }
 
@@ -19,7 +28,9 @@ public class SpeakerServiceImpl implements SpeakerService {
         return repository.findAll();
     }
 
+
     public void setRepository(SpeakerRepository repository) {
+        System.out.println("SpeakerServiceImpl setter");
         this.repository = repository;
     }
 }
