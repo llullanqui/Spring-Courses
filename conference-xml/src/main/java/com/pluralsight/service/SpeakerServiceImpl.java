9da673a4c429f2388cdce39b30264ede8cdc2442
llullanqui <luis.1142@hotmail.com>
2022-01-05 08:59:22 -0500

    finished xml configuration

--- conference-xml/src/main/java/com/pluralsight/service/SpeakerServiceImpl.java
+++ conference-xml/src/main/java/com/pluralsight/service/SpeakerServiceImpl.java
@@ -10,8 +10,18 @@ public class SpeakerServiceImpl implements SpeakerService {
 
     private SpeakerRepository repository = new HibernateSpeakerRepositoryImpl();
 
+    public SpeakerServiceImpl() {}
+
+    public SpeakerServiceImpl(SpeakerRepository repository) {
+        this.repository = repository;
+    }
+
     @Override
     public List<Speaker> findAll() {
         return repository.findAll();
+    }
+
+    public void setSpeakerRepository(SpeakerRepository repository) {
+        this.repository = repository;
     }
 }
